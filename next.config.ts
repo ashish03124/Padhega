e156c73edad3c67a3e3fa8134423badb0b5f6ffd
Ashish Garud <ashishgarud539@gmail.com>
2026-03-20 23:26:03 +0530

    fix: remove turbopack from build, upgrade cheerio

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['yt-search', 'cheerio'],
+  serverExternalPackages: ['yt-search', 'cheerio'],
   images: {
     remotePatterns: [
       {
